5cfbe60daef606cc15599d23457e6f62d73ed1ab
Georgelyn <georgelyn.dev@gmail.com>
2021-08-26 19:11:19 +0200

    Add basic web app to connect to API

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+api_uri = 'http://127.0.0.1:5001'
+
+@app.route('/')
+def index():
+    response = requests.get(api_uri + '/books').json()
+    books = response["books"]
+    return render_template('index.html', books=books)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
